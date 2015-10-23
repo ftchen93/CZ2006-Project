637f0718c8643b7fc3d68b8cf917cc143dcbffd5
ftchen93 <ft_chen93@hotmail.com>
2015-10-24 01:17:30 +0800

    merge conflict

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,35 @@
 angular.module('starter.controllers', [])
+.controller('HomeCtrl' , function ($scope){
+    $scope.picture = {
+      "artworks": [
+        {
+          "img_about_us": "img/aboutus.jpg",
+          "href": "tab.about",
+          "title": "about us"
+        },
+        {
+          "img_bg": "img/background.jpg"
+        },
+        {
+          "img_plan_activity": "img/planactivity.jpg",
+          "href": "tab.about",
+          "title": "Plan Activity"
+        },
+        {
+          "img_settings": "img/settings.jpg",
+          "href": "tab.settings",
+          "title": "Settings"
+        },
+        {
+          "img_weather_now": "img/weathernow.jpg",
+          "href": "tab.weather",
+          "title": "Weather Now"
+        }
 
+
+      ]
+    };
+  })
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -15,15 +45,48 @@ angular.module('starter.controllers', [])
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
+ 
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+  
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
+    enableFriends: false
   };
 })
-.controller('WeatherCtrl', function($scope) {})
+.controller('WeatherCtrl', function($scope, $ionicSlideBoxDelegate) {
+
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+
+  $scope.navSlide= function(index) {
+    $ionicSlideBoxDelegate.slide(index,500);
+  };
+})
+
+.controller('NowcastCtrl', function($scope) {})
+.controller('HistoryViewCtrl', function($scope, $rootScope) {
+  $scope.clearViewHistory = function() {
+    console.log('clearViewHistory');
+      $rootScope.$viewHistory = {
+      histories: { root: { historyId: 'root', parentHistoryId: null, stack: [], cursor: -1 } },
+      backView: null,
+      forwardView: null,
+      currentView: null,
+      disabledRegistrableTagNames: []
+    };
+  } 
+});
