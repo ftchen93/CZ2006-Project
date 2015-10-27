a98c16528e8c55ccb5d399bcbe41444f43a52b40
Zhou <zhou0235@e.ntu.edu.sg>
2015-10-27 12:45:48 +0800

    remove util

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','xml','ionic.utils','ionic.weather.directives'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','xml','ionic.utils'])
 
 
 .run(function($ionicPlatform, $rootScope) {
